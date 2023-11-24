70ffd1b7c9eafdc29eab7eaaf621262dab1c48c1
nimjeprathamesh <144525633+nimjeprathamesh@users.noreply.github.com>
2023-11-24 18:50:31 +0530

    Delete section 3 simple web page project directory

--- section 3 simple web page project/src/index.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom/client";
-
-import App from "./App.js";
-import "./index.css";
-
-const entryPoint = document.getElementById("root");
-ReactDOM.createRoot(entryPoint).render(<App />);
