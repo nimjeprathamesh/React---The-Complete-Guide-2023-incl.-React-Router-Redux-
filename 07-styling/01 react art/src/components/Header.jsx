65360d7b8d26d3c2abe297c5bb172c3d778cf914
nimjeprathamesh <144525633+nimjeprathamesh@users.noreply.github.com>
2023-12-01 19:41:46 +0530

    files upload
    part 1 of 7th section is completed

---
+++ 07-styling/01 react art/src/components/Header.jsx
@@ -0,0 +1,52 @@
+// import { styled } from 'styled-components';
+import logo from '../assets/logo.png';
+
+// const StyledHeader = styled.header`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+//   margin-bottom: 2rem;
+
+//   & img {
+//     object-fit: contain;
+//     margin-bottom: 2rem;
+//     width: 9rem;
+//     height: 8rem;
+//   }
+
+//   & h1 {
+//       font-size: 1.5rem;
+//       font-weight: 600;
+//       letter-spacing: 0.4em;
+//       text-align: center;
+//       text-transform: uppercase;
+//       color: #9a3412;
+//       font-family: 'Pacifico', cursive;
+//       margin: 0;
+//   }
+
+//   & p {
+//       text-align: center;
+//       color: #a39191;
+//       margin: 0;
+//   }
+
+//   @media (min-width: 768px) {
+//     & h1 {
+//         font-size: 2.25rem;
+//     }
+//   }
+// `;
+
+export default function Header() {
+  return (
+    <header className="flex flex-col items-center mt-6 mb-8">
+      <img src={logo} alt="A canvas" className='mb-8 w-44 h-44' />
+      <h1 className='text-xl md:text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title'>
+        ReactArt
+      </h1>
+      <p className='text-stone-500'>A community of artists and art-lovers.</p>
+    </header>
+  );
+}
