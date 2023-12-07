96bf89aee97ecdc3f4349e68676b819609195c06
nimjeprathamesh <144525633+nimjeprathamesh@users.noreply.github.com>
2023-12-07 18:02:39 +0530

    files upload

--- 08 Refs Portals/Project 1/src/components/ResultModal.jsx
+++ 08 Refs Portals/Project 1/src/components/ResultModal.jsx
@@ -1,7 +1,7 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 import { createPortal } from 'react-dom';
 
-const ResultModal = forwardRef(function ResultModal({targetTime, remainingTime, onReset}, ref) {
+const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime, onReset }, ref) {
     const dialog = useRef();
 
     const userLost = remainingTime <= 0;
@@ -11,19 +11,26 @@ const ResultModal = forwardRef(function ResultModal({targetTime, remainingTime, 
     useImperativeHandle(ref, () => {
         return {
             open() {
-                dialog.current.showModal();
+                dialog.current && dialog.current.showModal();
+            },
+            close() {
+                dialog.current && dialog.current.close();
             }
         };
-    });
+    }, []);
 
-    return createPortal (
-        <dialog ref={ref} className="result-modal">
+    return createPortal(
+        <dialog ref={dialog} className="result-modal">
             {userLost && <h2>You lost</h2>}
             {!userLost && <h2>Your Score: {score}</h2>}
             <p>The target time was <strong>{targetTime} seconds</strong></p>
             <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
-            <form method="dialog" onSubmit={onReset}>
-                <button>Close</button>
+            <form method="dialog" onSubmit={(e) => {
+                e.preventDefault();
+                onReset();
+                ref.current.close(); // Close the dialog explicitly
+            }}>
+                <button type="submit">Close</button>
             </form>
         </dialog>,
         document.getElementById('modal')
