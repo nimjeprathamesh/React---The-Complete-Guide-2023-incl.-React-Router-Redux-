518e379a5ee2f7d86c335f04bac3660b8de87902
nimjeprathamesh <144525633+nimjeprathamesh@users.noreply.github.com>
2024-01-10 19:34:11 +0530

    files upload
    1st project of this section is completed.

---
+++ 15 HTTP Requests/01 Place Picker/src/http.js
@@ -0,0 +1,42 @@
+export async function fetchAvailablePlaces() {
+    const response = await fetch('http://localhost:3000/places');
+    const resData = await response.json();
+
+    if(!response.ok) {
+        const error = new Error('Failed to fetch places');
+        throw error;
+    }
+
+    return resData.places;
+}
+
+export async function fetchUserPlaces() {
+    const response = await fetch('http://localhost:3000/user-places');
+    const resData = await response.json();
+
+    if(!response.ok) {
+        const error = new Error('Failed to fetch user places');
+        throw error;
+    }
+
+    return resData.places;
+}
+
+export async function updateUserPlaces(places) {
+    const response = await fetch('http://localhost:3000/user-places', {
+        method: 'PUT',
+        body: JSON.stringify({places}),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const resData = await response.json();
+
+    if(!response.ok) {
+        const error = new Error('Failed to update user data');
+        throw error;
+    }
+
+    return resData.message;
+}
