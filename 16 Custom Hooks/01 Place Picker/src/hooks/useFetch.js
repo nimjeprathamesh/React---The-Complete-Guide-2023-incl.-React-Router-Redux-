ba8e18d91e16d36f245f73c4494b1d407c95fa3d
nimjeprathamesh <nimjeprathamesh1@gmail.com>
2024-01-12 20:06:29 +0530

    files upload
    task completed

---
+++ 16 Custom Hooks/01 Place Picker/src/hooks/useFetch.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+export default function useFetch(fetchFn, initialValue) {
+    const [isFetching, setIsFetching] = useState();
+    const [error, setError] = useState();
+    const [fetchedData, setFetchedData] = useState(initialValue);
+
+    useEffect(() => {
+        async function fetchPlaces() {
+            setIsFetching(true);
+            try {
+                const data = await fetchFn();
+                setFetchedData(data);
+            } catch (error) {
+                setError({ message: error.message || 'Failed to fetch user places.' });
+            }
+
+            setIsFetching(false);
+        }
+
+        fetchPlaces();
+    }, [fetchFn]);
+
+    return {
+        isFetching,
+        fetchedData,
+        setFetchedData,
+        error
+    }
+}
