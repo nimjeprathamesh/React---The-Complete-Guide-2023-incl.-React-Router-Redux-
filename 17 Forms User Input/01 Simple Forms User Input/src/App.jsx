519b562619b594d9b27ebfb41273855ae9c8a7e0
nimjeprathamesh <nimjeprathamesh1@gmail.com>
2024-01-17 20:45:59 +0530

    file upload
    section completed

---
+++ 17 Forms User Input/01 Simple Forms User Input/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/Header.jsx';
+// import SignUp from './components/SignUp.jsx';
+import Login from './components/Login.jsx';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Login />
+        {/* <SignUp /> */}
+      </main>
+    </>
+  );
+}
+
+export default App;
