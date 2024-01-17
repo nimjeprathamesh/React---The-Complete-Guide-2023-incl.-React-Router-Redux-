7b79be3a803553e3368d7dd7e45832fbe82eb8fc
nimjeprathamesh <nimjeprathamesh1@gmail.com>
2024-01-17 20:54:18 +0530

    files upload
    section completed

---
+++ 17 Forms User Input/02 Complex Forms User Input/src/App.jsx
@@ -0,0 +1,12 @@
+import SimpleInput from './components/SimpleInput';
+
+function App() {
+  return (
+    <div className="app">
+      <SimpleInput />
+      {/* <BasicForm /> */}
+    </div>
+  );
+}
+
+export default App;
