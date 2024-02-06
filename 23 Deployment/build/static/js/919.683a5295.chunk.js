64226d9dcfb4db2f39503dc504597ed6ce6e14ac
nimjeprathamesh <nimjeprathamesh1@gmail.com>
2024-02-06 14:02:16 +0530

    files upload
    Section is completed.

---
+++ 23 Deployment/build/static/js/919.683a5295.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_temp=self.webpackChunkreact_temp||[]).push([[919],{919:(t,s,e)=>{e.r(s),e.d(s,{default:()=>i,loader:()=>a});var c=e(560),n=e(12);const o="PostList_list__ClsTb";var r=e(496);const l=function(t){let{posts:s}=t;return(0,r.jsx)("ul",{className:o,children:s.map((t=>(0,r.jsx)("li",{children:(0,r.jsx)(n.cH,{to:t.id.toString(),children:t.title})},t.id)))})};const i=function(){const t=(0,c.GY)();return(0,r.jsx)(l,{posts:t})};function a(){return fetch("https://jsonplaceholder.typicode.com/posts")}}}]);
+//# sourceMappingURL=919.683a5295.chunk.js.map
