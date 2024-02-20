f59290fbf817c17a2fb1c34e19f5c32b41581050
nimjeprathamesh <nimjeprathamesh1@gmail.com>
2024-02-20 20:29:50 +0530

    files upload
    This section is completed.

---
+++ 29 Testing React Apps/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Async from './components/Async';
+
+function App() {
+  return (
+    <div data-testid="async-component" className="App">
+      <Async />
+    </div>
+  );
+}
+
+export default App;
